02fde806d894367c24bc7556ef6bc78aa9318cac
Tomer Aberbach <tomer@aberba.ch>
2025-09-01 19:43:18 -0400

    feat: overhaul and require node 22

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,21 +1,6 @@
-/**
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import { test } from 'tomer'
-import etz from '../src/index.js'
+import { test } from '@fast-check/vitest'
+import { afterEach, expect } from 'vitest'
+import etz from './index.js'
 
 const { env } = process
 const { log } = console
@@ -88,7 +73,7 @@ test.each([
   [`errorEtz`, { env: { ETZ: `eRror` }, expectedMessages: [`ERROR`] }],
   [`errorIndexEtz`, { env: { ETZ: `3` }, expectedMessages: [`ERROR`] }],
   [`noEtzAndEtz`, { env: { NO_ETZ: `true`, ETZ: `0` }, expectedMessages: [] }],
-])(`etz %p`, (_, { env, expectedMessages }) => {
+])(`etz %s`, (_, { env, expectedMessages }) => {
   process.env = env
   const messages: string[] = []
   console.log = (message: string) => void messages.push(message)
@@ -99,8 +84,8 @@ test.each([
   etz.error(`Hello World!`)
 
   expect(messages).toHaveLength(expectedMessages.length)
-  expectedMessages.forEach((expectedMessage, index) => {
+  for (const [index, expectedMessage] of expectedMessages.entries()) {
     expect(messages[index]).toInclude(expectedMessage)
     expect(messages[index]).toInclude(`Hello World!`)
-  })
+  }
 })
